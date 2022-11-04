6dc7e82e4099ab9962ed4f1fa65bef3dea3b8533
acuyxzy <102140742+acuyxzy@users.noreply.github.com>
2022-11-04 20:51:26 +0700

    Add files via upload

--- plugins/game/spin.js
+++ plugins/game/spin.js
@@ -7,29 +7,29 @@ exports.run = {
    }) => {
       let user = global.db.users[m.sender]
       if (command == 'spinall') {
-         let reward = Func.randomInt(100, ((1 / 100) * (user.point * 2)))
-         user.point = 0
-         user.point += reward
-         let last = user.point
+         let reward = Func.randomInt(100, ((1 / 100) * (user.uang * 2)))
+         user.uang = 0
+         user.uang += reward
+         let last = user.uang
          let teks = `❏  *S P I N - R E S U L T*\n\n`
-         teks += `	*- ${Func.formatNumber(user.point)}*\n`
+         teks += `	*- ${Func.formatNumber(user.uang)}*\n`
          teks += `	*+ ${Func.formatNumber(reward)}*\n\n`
-         teks += `• *Total* : ${Func.formatNumber(last)} Point\n\n`
+         teks += `• *Total* : ${Func.formatNumber(last)}\n\n`
          teks += `*NB : “Anti-Spam jeda ${global.cooldown} detik untuk eksekusi selanjutnya.”*`
          client.reply(m.chat, teks, m)
       } else {
-         if (!args || !args[0] || args[0].startsWith('0')) return client.reply(m.chat, Func.texted('bold', `🚩 Berikan argumen berupa nominal point untuk dispin.`), m)
+         if (!args || !args[0] || args[0].startsWith('0')) return client.reply(m.chat, Func.texted('bold', `🚩 Berikan argumen berupa nominal uang untuk dispin.`), m)
          if (isNaN(args[0])) return client.reply(m.chat, Func.example(isPrefix, command, '10000'), m)
-         if (args[0] > user.point) return client.reply(m.chat, Func.texted('bold', `🚩 Pointmu tidak cukup untuk melakukan spin sebanyak ${Func.formatNumber(args[0])} point.`), m)
-         if (args[0] < 1000) return client.reply(m.chat, Func.texted('bold', `🚩 Tidak bisa melakukan spin dengan nominal dibawah 1000 point.`), m)
-         user.point -= args[0]
+         if (args[0] > user.uang) return client.reply(m.chat, Func.texted('bold', `🚩 uangmu tidak cukup untuk melakukan spin sebanyak ${Func.formatNumber(args[0])} uang.`), m)
+         if (args[0] < 1000) return client.reply(m.chat, Func.texted('bold', `🚩 Tidak bisa melakukan spin dengan nominal dibawah 1000 uang.`), m)
+         user.uang -= args[0]
          let reward = Func.randomInt(100, args[0] * 3)
-         user.point += reward
-         let last = user.point
+         user.uang += reward
+         let last = user.uang
          let teks = `❏  *S P I N - R E S U L T*\n\n`
          teks += `	*- ${Func.formatNumber(args[0])}*\n`
          teks += `	*+ ${Func.formatNumber(reward)}*\n\n`
-         teks += `• *Total* : ${Func.formatNumber(last)} Point\n\n`
+         teks += `• *Total* : ${Func.formatNumber(last)}\n\n`
          teks += `*NB : “Anti-Spam jeda ${global.cooldown} detik untuk eksekusi selanjutnya.”*`
          client.reply(m.chat, teks, m)
       }
