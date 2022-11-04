426ef188c2d96ab6ac3e1ac0c1b36f6c2dcb77ad
acuyxzy <102140742+acuyxzy@users.noreply.github.com>
2022-11-04 20:41:34 +0700

    Add files via upload

--- plugins/download/tiktok.js
+++ plugins/download/tiktok.js
@@ -1,7 +1,5 @@
 exports.run = {
-   usage: ['tiktok', 'tikmp3', 'tikwm'],
-   use: 'link',
-   category: 'downloader',
+   usage: ['tiktok', 'tmp3', 'twm'],
    async: async (m, {
       client,
       args,
@@ -11,25 +9,25 @@ exports.run = {
       try {
          if (!args || !args[0]) return client.reply(m.chat, Func.example(isPrefix, command, 'https://vt.tiktok.com/ZSe22y3dA'), m)
          if (!args[0].match('tiktok.com')) return client.reply(m.chat, global.status.invalid, m)
-         client.sendReact(m.chat, '🕒', m.key)
+         client.reply(m.chat, global.status.getdata, m)
          let old = new Date()
          let json = await Api.tiktok(Func.ttFixed(args[0]))
          if (!json.status) return client.reply(m.chat, Func.jsonFormat(json), m)
-         if (command == 'tiktok') return client.sendButton(m.chat, json.data.video, `If you want to get the *original sound* press the button below.\n🍟 *Fetching* : ${((new Date - old) * 1)} ms`, ``, m, [{
-            buttonId: `${isPrefix}tikmp3 ${args[0]}`,
+         if (command == 'tiktok') return client.sendButton(m.chat, json.data.video, `dari : ${json.author}\n${json.caption}\n\n*Mengambil* : ${((new Date - old) * 1)} detik`, ``, m, [{
+            buttonId: `${isPrefix}extract ${json.data.video}`,
             buttonText: {
-               displayText: 'Backsound'
+               displayText: 'Audio'
             },
             type: 1
          }])
-         if (command == 'tikwm') return client.sendButton(m.chat, json.data.videoWM, `If you want to get the *original sound* press the button below.\n🍟 *Fetching* : ${((new Date - old) * 1)} ms`, ``, m, [{
+         if (command == 'twm') return client.sendButton(m.chat, json.data.videoWM, `*Mengambil* : ${((new Date - old) * 1)} detik`, ``, m, [{
             buttonId: `${isPrefix}tikmp3 ${args[0]}`,
             buttonText: {
-               displayText: 'Backsound'
+               displayText: 'Audio'
             },
             type: 1
          }])
-         if (command == 'tikmp3') return !json.data.audio ? client.reply(m.chat, global.status.fail, m) : client.sendFile(m.chat, json.data.audio, 'audio.mp3', '', m)
+         if (command == 'tmp3') return !json.data.audio ? client.reply(m.chat, global.status.fail, m) : client.sendFile(m.chat, json.data.audio, 'audio.mp3', '', m)
       } catch (e) {
          console.log(e)
          return client.reply(m.chat, global.status.error, m)
