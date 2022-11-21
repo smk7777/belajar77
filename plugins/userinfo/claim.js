abf480e36f7277c4443875bfb626d3c03c4d6c49
acuyxzy <102140742+acuyxzy@users.noreply.github.com>
2022-11-21 07:58:16 +0700

    Add files via upload

--- plugins/userinfo/claim.js
+++ plugins/userinfo/claim.js
@@ -3,24 +3,22 @@ exports.run = {
    async: async (m, {
       client
    }) => {
-      let user = global.db.users[m.sender]
+      let user = global.users[m.sender]
       let timeClaim = 3600000
       let claimed = new Date(user.lastclaim + timeClaim)
       let timeout = claimed - new Date()
       if (new Date - user.lastclaim > timeClaim) {
-         client.sendMessageModify(m.chat, Func.texted('bold', `Selamat, kamu mendapat Uang 1JT dan 10 limit 🍟️`), m, {
+         client.sendMessageModify(m.chat, Func.texted('bold', `Selamat, kamu mendapat Uang 10JT dan 50 limit 🔮️`), m, {
             largeThumb: true,
-            thumbnail: await Func.fetchBuffer('https://telegra.ph/file/726f6f808f64b3a3f6ce2.jpg'),
-            url: 'https://chat.whatsapp.com/EARvthLENgw2yxhDUKDuMr'
+            thumbnail: await Func.fetchBuffer('https://telegra.ph/file/e4954937673ef75e467b2.jpg')
          })
-         global.db.users.uang += 1000000
-         user.limit += 10
+         user.uang += 10000000
+         user.limit += 50
          user.lastclaim = new Date() * 1
       } else {
          client.sendMessageModify(m.chat, Func.texted('bold', `Kamu sudah melakukan claim silahkan klaim kembali di jam berikutnya*\n\n*⏱️ : ${Func.toTime(timeout)}️`), m, {
             largeThumb: true,
-            thumbnail: await Func.fetchBuffer('https://telegra.ph/file/71465e68c23dc8cf9de69.jpg'),
-            url: 'https://chat.whatsapp.com/EARvthLENgw2yxhDUKDuMr'
+            thumbnail: await Func.fetchBuffer('https://telegra.ph/file/2d7a56edbda6128633a06.jpg')
          })
       }
    },
