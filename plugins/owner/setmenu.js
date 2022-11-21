443e974a00ec5a01aabccdf587ef387b14b52055
acuyxzy <102140742+acuyxzy@users.noreply.github.com>
2022-11-21 07:46:55 +0700

    Add files via upload

--- plugins/owner/setmenu.js
+++ plugins/owner/setmenu.js
@@ -12,19 +12,43 @@ exports.run = {
          let setting = global.db.setting
          if (!args || !args[0]) {
             let rows = [{
-               title: `STYLE 1`,
+               title: `Umum`,
                rowId: `${isPrefix + command} 1`,
                description: ''
             }, {
-               title: `STYLE 2`,
+               title: `Fake Reply Story`,
                rowId: `${isPrefix + command} 2`,
+               description: ''
+            }, {
+               title: `Menu Document`,
+               rowId: `${isPrefix + command} 3`,
+               description: ''
+            }, {
+               title: `Menu Location`,
+               rowId: `${isPrefix + command} 4`,
+               description: ''
+            }, {
+               title: `Menu GIF`,
+               rowId: `${isPrefix + command} 5`,
+               description: ''
+            }, {
+               title: `Menu Image`,
+               rowId: `${isPrefix + command} 6`,
+               description: ''
+            }, {
+               title: `Menu Video`,
+               rowId: `${isPrefix + command} 7`,
+               description: ''
+            }, {
+               title: `Menu List`,
+               rowId: `${isPrefix + command} 8`,
                description: ''
             }]
             client.sendList(m.chat, '', `Choose menu style. 🍟`, '', 'Tap!', [{
                rows
             }], m)
          } else {
-            client.reply(m.chat, `🚩 Bot menu successfully set using style *${args[0]}*.`, m).then(() => setting.menuStyle = parseInt(args[0]))
+            client.reply(m.chat, `🚩 Menu berhasil disetel menggunakan gaya *${args[0]}*.`, m).then(() => setting.setmenu = parseInt(args[0]))
          }
       } catch (e) {
          console.log(e)
