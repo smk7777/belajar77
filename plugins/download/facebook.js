7b2aae2c3bdf56984337d7c8491dc0d81b85ea51
acuyxzy <102140742+acuyxzy@users.noreply.github.com>
2022-11-22 21:28:17 +0700

    Update facebook.js

--- plugins/download/facebook.js
+++ plugins/download/facebook.js
@@ -12,7 +12,7 @@ exports.run = {
       try {
          if (!args || !args[0]) return client.reply(m.chat, Func.example(isPrefix, command, 'https://fb.watch/7B5KBCgdO3'), m)
          if (!args[0].match(/(?:https?:\/\/(web\.|www\.|m\.)?(facebook|fb)\.(com|watch)\S+)?$/)) return client.reply(m.chat, global.status.invalid, m)
-         client.sendReact(m.chat, '🕒', m.key)
+         client.reply(m.chat, global.status.getdata, m)
          let json = await Api.fb(args[0])
          if (!json.status) return client.reply(m.chat, Func.jsonFormat(json), m)
          let result = json.data.find(v => v.quality == 'HD')
