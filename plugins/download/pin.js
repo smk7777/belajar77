9dfd6ee4f161fa14e92d7b70e5a19ce58fd87492
acuyxzy <102140742+acuyxzy@users.noreply.github.com>
2022-11-22 21:29:11 +0700

    Update pin.js

--- plugins/download/pin.js
+++ plugins/download/pin.js
@@ -11,7 +11,7 @@ exports.run = {
       try {
          if (!args || !args[0]) return client.reply(m.chat, Func.example(isPrefix, command, 'https://pin.it/5fXaAWE'), m)
          if (!args[0].match(/pin(?:terest)?(?:\.it|\.com)/)) return client.reply(m.chat, global.status.invalid, m)
-         client.sendReact(m.chat, '🕒', m.key)
+         client.reply(m.chat, global.status.getdata, m)
          let old = new Date()
          let json = await Api.pin(args[0])
          if (!json.status) return client.reply(m.chat, Func.jsonFormat(json), m)
